6136deef6b60478ab01deb91dc345b931332a144
jinhobae1 <pookgoon@naver.com>
2018-03-20 10:35:34 +0900

    출구를 두개로 변경

--- src/main/java/refactoring/chap02/FindInt.java
+++ src/main/java/refactoring/chap02/FindInt.java
@@ -2,8 +2,7 @@ package refactoring.chap02;
 
 public class FindInt {
 	public static boolean find(int[] date, int target) {
-		boolean found = false;
-		for (int i = 0; i < date.length && !found; i++) {
+		for (int i = 0; i < date.length; i++) {
 			if (date[i] == target) {
 				return true;
 			}
