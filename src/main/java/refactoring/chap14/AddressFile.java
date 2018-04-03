d2238281f29e563641b2a50857cd334dfad2d132
jinhobae1 <pookgoon@naver.com>
2018-04-03 14:50:00 +0900

    1회 리펙토링

--- src/main/java/refactoring/chap14/AddressFile.java
+++ src/main/java/refactoring/chap14/AddressFile.java
@@ -1,15 +1,27 @@
 package refactoring.chap14;
 
+import java.io.IOException;
 import java.util.Enumeration;
 
 public class AddressFile {
 	private final Database database;
+
 	public AddressFile(String filename) {
 		database = new Database(filename);
 	}
-	public Database getDatabase() {
-		return database;
+
+	public void set(String key, String value) {
+		database.set(key, value);
 	}
+
+	public String get(String key) {
+		return database.get(key);
+	}
+
+	public void update() throws IOException {
+		database.update();
+	}
+
 	public Enumeration names() {
 		return database.getProperties().propertyNames();
 	}
--- src/main/java/refactoring/chap14/Main.java
+++ src/main/java/refactoring/chap14/Main.java
@@ -7,15 +7,15 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			AddressFile file = new AddressFile("address.txt");
-			file.getDatabase().set("Hiroshi yuki","hyuki@example.com");
-			file.getDatabase().set("Hiroshi yuki2","hyuki2@example.com");
-			file.getDatabase().set("Hiroshi yuki3","hyuki3@example.com");
-			file.getDatabase().update();
+			file.set("Hiroshi yuki","hyuki@example.com");
+			file.set("Hiroshi yuki2","hyuki2@example.com");
+			file.set("Hiroshi yuki3","hyuki3@example.com");
+			file.update();
 			
 			Enumeration e = file.names();
 			while(e.hasMoreElements()) {
 				String name = (String)e.nextElement();
-				String mail = file.getDatabase().get(name);
+				String mail = file.get(name);
 				System.out.println("name"+name+",mail="+mail);
 				
 			}
