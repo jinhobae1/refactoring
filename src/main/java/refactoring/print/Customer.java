d0dc232b2568ffe05131eccd226c6afaf0c54d37
jinhobae1 <pookgoon@naver.com>
2018-04-09 11:23:37 +0900

    메서드 이동 적용 Customer - > Rental

--- src/main/java/refactoring/print/Customer.java
+++ src/main/java/refactoring/print/Customer.java
@@ -26,23 +26,7 @@ public class Customer {
 		StringBuilder result = new StringBuilder(getName() + "°í°´´ÔÀÇ ´ë¿© ±â·Ï\n");
 
 		for (Rental each : rentals) {
-			double thisAmount = 0;
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
+			double thisAmount = each.getCharge();
 			frequentRenterPoints++;
 			if((each.getMovie().getPriceCode()==Movie.NEW_RELEASE)&&each.getDaysRented()>1)
 				frequentRenterPoints++;
